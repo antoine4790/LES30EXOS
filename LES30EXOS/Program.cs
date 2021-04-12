816c22f55a94db856f870805012d93740c3951c8
antoine4790 <antoine-41@live.fr>
2021-04-12 09:55:45 +0200

    Ajout de la fonction biggestNumber et ajout de texte dans la console pour différencier les fonctions à l'execution

--- LES30EXOS/Program.cs
+++ LES30EXOS/Program.cs
@@ -14,15 +14,19 @@ namespace LES30EXOS
             //Console.WriteLine("voici votre valeur :" + GetIntegerFromCmdLine());
 
             //Exo3
+            //Console.WriteLine("Inverse deux nombres");
             //Console.WriteLine(SwitchOrder());
 
             //Exo4 
+            //Console.WriteLine("Affiche si les 2 nombres sont égaux");
             //Console.WriteLine(DisplayEqualNumbers());
 
             //Exo5
+            //Console.WriteLine("Affiche inverse et affiche 2 nombres");
             //Console.WriteLine(DisplaySwitchAndDisplay2Numbers());
 
             //Exo6
+            //Console.WriteLine("Operations Addition Soustraction Division Multiplication");
             //float[] ArrayResults= AddSubstractDivideMultiple2Numbers();
             //for (int i = 0; i < ArrayResults.Length; i++)
             //{
@@ -30,10 +34,36 @@ namespace LES30EXOS
             //}
 
             //Exo7
-            Console.WriteLine("La moyenne est de " + DisplayMean());
+            //Console.WriteLine("Moyenne");
+            //Console.WriteLine("La moyenne est de " + DisplayMean());
 
+            //Exo8
+            //Console.WriteLine("Affiche le plus grand des nombres");
+            Console.WriteLine("Le plus grand nombre est :" + BiggestNumber());
         }
 
+        static float[] GetEntriesNumberFromCmdLine()
+        {
+            
+            Console.WriteLine("Combien de chiffres devez vous entrer?");
+
+            bool isOk= int.TryParse(Console.ReadLine(), out int EntriesNumber);
+            //if (isOk)
+            //{
+            //    float[] ArrayNumbers = new float[EntriesNumber];
+            //}
+            //else
+            //    return 1;
+
+            float[] ArrayNumbers= new float[EntriesNumber];
+            
+                for (int i = 0; i < EntriesNumber; i++)
+                {
+                    ArrayNumbers[i]=GetIntegerFromCmdLine("Entrez un chiffre");
+                }
+            
+            return ArrayNumbers;
+        }
         static void HelloWorld()
         {
             Console.WriteLine("Hello World!");
@@ -123,6 +153,20 @@ namespace LES30EXOS
             }
 
             return mean / ArrayNumbers.Length;
+        }
+
+        static float BiggestNumber()
+        {
+            float biggestNumber;
+            float[] Enb = GetEntriesNumberFromCmdLine();
+            biggestNumber = Enb[0];
+            for (int i = 1; i < Enb.Length; i++)
+            {
+                if (Enb[i] > biggestNumber)
+                    biggestNumber = Enb[i];
+            }
+
+            return biggestNumber;
         }
     }
 }
