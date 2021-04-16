91f117b10ee61f293d3c0d7925b7871e471a09b7
antoine4790 <antoine-41@live.fr>
2021-04-16 22:34:24 +0200

    Essai de test unitaire sur la division par zero Creation de la classe Tools

---
+++ ClassLibrary/Tools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Tools
+    {
+        public static double diviser(double nb1, double nb2)
+        {
+            try
+            {
+                if (nb2 != 0)
+                    return nb1 / nb2;
+                else
+                    throw new ExceptionDivision("Impossible de diviser par 0");
+            }
+            //gestion de la division par 0
+            catch (ExceptionDivision e)
+            {
+                Console.WriteLine($"Voici l'erreur {e} .");
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ ClassLibraryTests/ToolsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    [TestClass()]
+    public class ToolsTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ExceptionDivision))]
+        public void DiviserTest()
+        {
+            //Arrange
+            int numerateur = 5;
+            int denominateur = 0;
+
+            //Act
+            double result = Tools.diviser(numerateur, denominateur);
+
+            //Assert
+            //rien on attend juste une exception
+
+        }
+    }
+}
