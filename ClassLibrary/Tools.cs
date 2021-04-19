914a8d35a254424a47d43d9d3a530450ee905125
antoine4790 <antoine-41@live.fr>
2021-04-19 09:45:50 +0200

    First unit test OK!

--- ClassLibrary/Tools.cs
+++ ClassLibrary/Tools.cs
@@ -10,19 +10,12 @@ namespace ClassLibrary
     {
         public static double diviser(double nb1, double nb2)
         {
-            try
-            {
-                if (nb2 != 0)
-                    return nb1 / nb2;
-                else
-                    throw new ExceptionDivision("Impossible de diviser par 0");
-            }
-            //gestion de la division par 0
-            catch (ExceptionDivision e)
-            {
-                Console.WriteLine($"Voici l'erreur {e} .");
-                return 0;
-            }
+            
+            if (nb2 != 0)
+                return nb1 / nb2;
+            else
+                throw new ExceptionDivision("Impossible de diviser par 0");
+            
 
         }
     }
